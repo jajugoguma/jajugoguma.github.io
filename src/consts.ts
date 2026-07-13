bab33ea7f010af56d0f25d5f446b1edf658a9a84
daeyeon-kim <daeyeon.kim@nhn.com>
2026-07-14 08:07:27 +0900

    feat: switch theme to astro cactus with pretendard

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'jajugoguma';
-export const SITE_DESCRIPTION = '개발 블로그';
