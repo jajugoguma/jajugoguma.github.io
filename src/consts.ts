7c4509d04e710793be170a5260987edb6e80b17e
daeyeon-kim <daeyeon.kim@nhn.com>
2026-07-14 07:48:01 +0900

    feat: migrate blog from gatsby to astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'jajugoguma';
+export const SITE_DESCRIPTION = '개발 블로그';
